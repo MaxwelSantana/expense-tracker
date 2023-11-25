6da3cb2734d3ffd73f307a5d3e204cf1a20f41a3
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2023-11-25 17:33:05 -0500

    Added summary transactions component

---
+++ ExpenseTrackerClient/src/app/pages/ui-components/transactions-summary/transactions-summary/transactions-summary.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransactionsSummaryComponent } from './transactions-summary.component';
+
+describe('TransactionsSummaryComponent', () => {
+  let component: TransactionsSummaryComponent;
+  let fixture: ComponentFixture<TransactionsSummaryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TransactionsSummaryComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TransactionsSummaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ExpenseTrackerClient/src/app/pages/ui-components/transactions-summary/transactions-summary/transactions-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-summary',
+  templateUrl: './transactions-summary.component.html',
+  styleUrls: ['./transactions-summary.component.scss']
+})
+export class TransactionsSummaryComponent {
+
+}
