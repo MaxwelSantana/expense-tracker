767ea4835b75f89b9927a6df95f10a24d7f629b0
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2023-11-25 15:33:56 -0500

    handle the navigation within the component code itself

--- ExpenseTrackerClient/src/app/pages/extra/sample-page/sample-page.component.ts
+++ ExpenseTrackerClient/src/app/pages/extra/sample-page/sample-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Transaction } from 'src/app/models/transaction';
 import { RestDataSource } from 'src/app/services/rest.datasource';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,18 +10,29 @@ import { RestDataSource } from 'src/app/services/rest.datasource';
   templateUrl: './sample-page.component.html',
   styleUrls: ['./sample-page.component.css']
 })
-export class AppSamplePageComponent {  
-  
+export class AppSamplePageComponent {
+
   newTransaction : Transaction = new Transaction(); // Create a new instance of Transaction class
 
-  constructor(private restDataSource: RestDataSource) {}
+  constructor(private restDataSource: RestDataSource, private router: Router) {}
 
 
   onAddTransaction(form:NgForm) {
-    if (form.valid) {      
-      
+    console.log("calling onAddTransaction");
+    if (form.valid) {
+
+      console.log('New Transaction:', this.newTransaction);
+
+      //It sends the newTransaction data to the backend via the addTransaction method in the RestDataSource service.
+
       this.restDataSource.addTransaction(this.newTransaction).subscribe(t => {
         form.resetForm;
-      })    
-  }} 
+
+         // Reset the newTransaction object to prepare for the next transaction
+         this.newTransaction = new Transaction();
+
+         // Navigate to another route after successful transaction addition
+        this.router.navigate(['/ui-components/lists']);
+      })
+  }}
 }
