11627ae6d9eb1a3c72c98c6b3aebee5fe52f45cd
Selene <selenechhangur@gmail.com>
2023-12-03 19:36:55 -0500

    changes made to app.module.ts & income ts/html

--- ExpenseTrackerClient/src/app/pages/income/income.component.ts
+++ ExpenseTrackerClient/src/app/pages/income/income.component.ts
@@ -1,10 +1,46 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Transaction } from 'src/app/models/transaction';
+import { RestDataSource } from 'src/app/services/rest.datasource';
 
 @Component({
   selector: 'app-income',
-  templateUrl: './income.component.html',
+  templateUrl: './income.component.html'
 })
 export class IncomeComponent {
+
+  transactions ?: Transaction[] | null ;
   
+  constructor(private dataSource: RestDataSource) {
+    this.dataSource.getTransactions().subscribe(t=> {
+    this.transactions = t;
+    console.log(this.transactions);
+    })
+  }
+
+  sumIncome(transactions: Transaction[]): number {
+    let sumIncome = 0;
+
+      for(let i =0; i<transactions.length; i++){
+        if(transactions[i].status === "Received"){
+          sumIncome = sumIncome + this.calculateSubtotal(transactions[i])
+      }
+      }
+    return sumIncome;
+  }
+
+  calculateSubtotal(transaction: Transaction): number {
+    return transaction.quantity * transaction.amount;
+  }
+
+  sumPayment(transactions: Transaction[]): number {
+    let sumPayment = 0;
+
+      for(let i =0; i<transactions.length; i++){
+        if(transactions[i].status === "Payment"){
+          sumPayment = sumPayment + this.calculateSubtotal(transactions[i])
+      }
+      }
+    return sumPayment;
+  }
 }
