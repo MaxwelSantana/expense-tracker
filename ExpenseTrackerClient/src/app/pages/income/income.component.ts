adae5398264d3f55fedc660d5c4159e5a2926565
Selene <selenechhangur@gmail.com>
2023-12-05 20:56:18 -0500

    created table for income v expenses

--- ExpenseTrackerClient/src/app/pages/income/income.component.ts
+++ ExpenseTrackerClient/src/app/pages/income/income.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Transaction } from 'src/app/models/transaction';
 import { RestDataSource } from 'src/app/services/rest.datasource';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-income',
@@ -42,5 +43,21 @@ export class IncomeComponent {
       }
       }
     return sumPayment;
+  }
+
+  /** Default name for excel file when downloaded */
+  fileName= "ExcelSheet.xlsx"
+  exportExcel(){
+    /**passing table id */
+    let data = document.getElementById("table-data");
+    const ws:XLSX.WorkSheet = XLSX.utils.table_to_sheet(data);
+
+    /**Generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
+
+    /**save to file */
+    XLSX.writeFile(wb, this.fileName)
+
   }
 }
--- ExpenseTrackerClient/src/app/pages/pages.module.ts
+++ ExpenseTrackerClient/src/app/pages/pages.module.ts
@@ -26,6 +26,7 @@ import {
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BudgetSummaryComponent } from './budget/budget-summary/budget-summary.component';
+import { IncomeComponent } from './income/income.component';
 
 // See the Moment.js docs for the meaning of these formats:
 // https://momentjs.com/docs/#/displaying/format/
@@ -42,7 +43,7 @@ export const MY_FORMATS = {
 };
 
 @NgModule({
-  declarations: [AppDashboardComponent, BudgetComponent,BudgetSummaryComponent],
+  declarations: [AppDashboardComponent, BudgetComponent,BudgetSummaryComponent, IncomeComponent],
   providers: [
     // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
     // application's root module. We provide it at the component level here, due to limitations of
