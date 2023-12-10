8532c7b5a5d44e9a68e80d52832f56afb9b112a0
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2023-12-10 18:02:32 -0500

    Added unit tests for summary-transactions

--- ExpenseTrackerClient/src/app/pages/ui-components/transactions-summary/transactions-summary.component.spec.ts
+++ ExpenseTrackerClient/src/app/pages/ui-components/transactions-summary/transactions-summary.component.spec.ts
@@ -1,17 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { TransactionsSummaryComponent } from './transactions-summary.component';
+import { Transaction } from 'src/app/models/transaction';
+import { RestDataSource } from 'src/app/services/rest.datasource';
+import { of } from 'rxjs';
 
 describe('TransactionsSummaryComponent', () => {
   let component: TransactionsSummaryComponent;
   let fixture: ComponentFixture<TransactionsSummaryComponent>;
+  let mockDataSource: Partial<RestDataSource>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TransactionsSummaryComponent ]
-    })
-    .compileComponents();
+    mockDataSource = {
+      getTransactions: () => of([
+        // Mock transactions here if needed for tests
+      ])
+    };
 
+    await TestBed.configureTestingModule({
+      declarations: [TransactionsSummaryComponent],
+      providers: [{ provide: RestDataSource, useValue: mockDataSource }]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(TransactionsSummaryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +30,131 @@ describe('TransactionsSummaryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Test calculateSubtotal method
+  it('should calculate subtotal correctly', () => {
+    const transaction: Transaction = {
+      quantity: 5, amount: 10,
+      _id: '',
+      category: 'Transportation',
+      subcategory: 'TTC',
+      description: 'Student Monthly Pass',
+      status: 'Payment',
+      date: new Date(2023, 9, 30),
+      clearForm: function (): void {
+        throw new Error('Function not implemented.');
+      }
+    };
+    const subtotal = component.calculateSubtotal(transaction);
+    expect(subtotal).toEqual(50); // Modify the expectation based on your calculation logic
+  });
+
+  // Test sumIncome method
+  it('should calculate sum of income correctly', () => {
+    const mockTransactions: Transaction[] = [
+
+      {
+        quantity: 2, amount: 20,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Received',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      },
+
+
+      {
+        quantity: 3, amount: 15,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Payment',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      }
+    ];
+
+    const sumIncome = component.sumIncome(mockTransactions);
+    expect(sumIncome).toEqual(40); // Modify the expectation based on your calculation logic
+  });
+
+  // Test sumPayment method
+  it('should calculate sum of payment correctly', () => {
+    const mockTransactions: Transaction[] = [
+
+      {
+        quantity: 2, amount: 20,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Received',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      },
+
+
+      {
+        quantity: 3, amount: 15,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Payment',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      }
+    ];
+
+    const sumPayment = component.sumPayment(mockTransactions);
+    expect(sumPayment).toEqual(45); // Modify the expectation based on your calculation logic
+  });
+
+  // Test myBalance method
+  it('should calculate balance correctly', () => {
+        const mockTransactions: Transaction[] = [
+
+      {
+        quantity: 2, amount: 20,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Received',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      },
+
+
+      {
+        quantity: 3, amount: 15,
+        _id: '',
+        category: 'Transportation',
+        subcategory: 'TTC',
+        description: 'Student Monthly Pass',
+        status: 'Payment',
+        date: new Date(2023, 9, 30),
+        clearForm: function (): void {
+          throw new Error('Function not implemented.');
+        }
+      }
+    ];
+
+    const balance = component.myBalance(mockTransactions);
+    expect(balance).toEqual(-5); // Modify the expectation based on your calculation logic
   });
 });
